d56511c2b67d660b0eb3912d2f59ea0358d54ada
RoMANzhula <roman1985al@ukr.net>
2024-11-22 23:35:20 +0200

    Add ArrayListWithStreamAPI - add mapping methods

--- src/iterable_interface/collection_interfaces/a_list_interface/array_list_class_realization/ArrayListWithStreamAPI.java
+++ src/iterable_interface/collection_interfaces/a_list_interface/array_list_class_realization/ArrayListWithStreamAPI.java
@@ -2,6 +2,8 @@ package iterable_interface.collection_interfaces.a_list_interface.array_list_cla
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ArrayListWithStreamAPI {
 
@@ -9,8 +11,15 @@ public class ArrayListWithStreamAPI {
         ArrayList<String> fruits = new ArrayList<>(Arrays.asList("Banana", "Orange", "Apple", "Melon"));
         ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 5, 3, 8, 1, 7, 2, 4));
 
+                    // filtering
         filterByConditionFirstWord(fruits, "A"); // Apple
         countElementsByCondition(numbers, 1); // 2
+
+                    // mapping (transforming)
+        mappingToUpperCase(fruits); // BANANA ORANGE APPLE MELON (each in new row)
+        mappingToUpperCaseInOneLine(fruits); // BANANA, ORANGE, APPLE, MELON
+        mappingNumberToWords(numbers); // One Two Five Three Eight One Seven Two Four
+        mappingNumberToBinary(numbers); // 1 10 101 11 1000 1 111 10 100 (each in new row)
     }
 
                                 // FILTERS
@@ -18,7 +27,7 @@ public class ArrayListWithStreamAPI {
     private static void filterByConditionFirstWord(ArrayList<String> list, String word) {
         list.stream()
                 .filter(element -> element.startsWith(word))
-                .forEach(System.out::println)
+                .forEach(System.out::print)
         ;
     }
 
@@ -33,6 +42,45 @@ public class ArrayListWithStreamAPI {
 
                                 // TRANSFORMATION
 
+    private static void mappingToUpperCase(ArrayList<String> list) {
+        list.stream()
+                .map(String::toUpperCase)
+                .forEach(System.out::println)
+        ;
+    }
+
+    private static void mappingToUpperCaseInOneLine(ArrayList<String> list) {
+        String result = list.stream()
+                .map(String::toUpperCase)
+                .collect(Collectors.joining(", "));
+
+        System.out.println(result);
+    }
+
+    private static void mappingNumberToWords(ArrayList<Integer> list) {
+        Map<Integer, String> numberToWordMap = Map.of(
+                0, "Zero",
+                1, "One",
+                2, "Two",
+                3, "Three",
+                4, "Four",
+                5, "Five",
+                6, "Six",
+                7, "Seven",
+                8, "Eight",
+                9, "Nine"
+        );
+
+        list.stream()
+                .map(number -> numberToWordMap.getOrDefault(number, "Unknown"))
+                .forEach(System.out::println);
+    }
+
+    private static void mappingNumberToBinary(ArrayList<Integer> list) {
+        list.stream()
+                .map(Integer::toBinaryString)
+                .forEach(System.out::println);
+    }
 
 
 }
